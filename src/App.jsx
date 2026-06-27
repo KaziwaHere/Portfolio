9ddeb42bdb0771dec11291e875a8af2bc2b8cf23
Kaziwa <143758469+KaziwaHere@users.noreply.github.com>
2026-06-27 15:38:05 +0300

    base

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+      <section className="hero">
+        <div className="hero_text">
+          <h1>Kaziwa Yousif Mohammed</h1>
+          <h2>Developer</h2>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
